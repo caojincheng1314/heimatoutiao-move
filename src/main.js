6e02c2feb781c28035b12a1bbbab2d73cdce00c5
岁月静好 <277626493@qq.com>
2021-06-27 18:26:54 +0800

    项目初始化完成

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './style/index.less'
 
+// 引入vant组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
